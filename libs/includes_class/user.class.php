c88c809490453a162d4704e348d2812d0de41f8f
gowtham <Gowtham032@essentials.selfmade.ninja>
2022-10-16 13:04:53 +0000

    getter setter is added

--- libs/includes_class/user.class.php
+++ libs/includes_class/user.class.php
@@ -2,10 +2,21 @@
 
 
 class user{
+  public static $id=null;
 public function __construct($username){
 
 $this->conn= database::get_connection();
-$this->conn->query();
+$this->username=$username;
+$sql="SELECT * FROM `photogram_login` WHERE `username` = '$username' LIMIT 1";
+$result=$this->conn->query($sql);
+if($result->num_rows){
+  $rows=$result-> fetch_assoc();
+  $this->is=$rows['id'];
+  $id=$is;
+
+}
+
+
 
 }
 
@@ -66,16 +77,84 @@ $result = $conn -> query($quer);
 
  database::$conn->close();
 }
+private static function getdata($var){
+  if(!$this->conn){
+    $this->conn=database::get_connection();
+  }
+  $quer="SELECT `$var` FROM `user_credentials` WHERE `id` = '$this->id' LIMIT 1";
+  $result=$this->conn->query($quer);
+
+  if($result->num_rows){
+    return $result->fetchassoc()[$var];
+  }else{
+    return null;
+  }
+  
+
+}
+private static function setdata($var,$value){
+  if(!$this->conn){
+    $this->conn=database::get_connection();
+  }
+  $sql = "UPDATE `users` SET `$var`='$data' WHERE `id`='$this->id'";
+  if($this->conn->query($sql)){
+    return true;
+  }else{
+    return false;
+  }
+}
+public function getavatar(){
+return $this->getdata('avatar');
+}
+public function setavatar($value){
+ return $this->setdata('avatar',$value);
+}
+public function setbio($value){
+  return $this->setdata('bio',$value);
+}
+public function getbio(){
+  return $this->getdata('bio');
+}
+public function setfirstname($value){
+  return $this->setdata('firstname',$value);
+}
+public function getfirstname(){
+  return $this->getdata('firstnmae');
+}
+public function getlastname(){
+  return $this->getdata('lastname');
+}
+public function setlastname($value){
+  return $this->setdata('lastname',$value);
+}
+public function getdob(){
+  return $this->getdata('dob');
+}
+public static function setdob($value){
+  return $this->setdata('dob',$value);
+}
+public static function getinstaid(){
+  return $this->getdata('instagram');
+}
+public static function setinstaid($value){
+  return $this->setdata('instagram',$value);
+}
+public static function gettwitterid(){
+  return $this->getdata('twitter');
+}
+public static function settwitterid($value){
+  return $this->setdata('twitter',$value);
+}
+public static function setlinkedinid(){
+  //todo update this option
+}
+public static function getlinkedinid(){
+  
+}
 public function __destruct()
 {
   $this->conn->close();
 }
 
-public static function setavatar(){
-
-}
-public static function setbio(){
-  
-}
 }
 ?>
--- test.php
+++ test.php
@@ -1,26 +1,7 @@
-<pre>
-    <?php
-   include 'libs/load.php';
-    print("_SERVER"); 
-    print_r($_SERVER);
-    print("_get"); 
-    print_r($_GET);
-     print("_POST"); 
-    print_r($_POST);
-     print("_COOKIE"); 
-    print_r($_COOKIE);
- if (user::signup("sadha","gowtham@gmail.com","9393939988","gowtham@@@2003")){
-    echo "success";
- }else{
-     echo "failed";
- }
-$mi1=new mic();
-$mi2=new mic();
+<?php
+include 'libs/load.php';
 
-$mi1->brand="tdjdjd";
-$mi2->brand="hdjfh";
-$mi1->color="RGB";
-$mi1->setcolor("white");
-
+$obj = new user('gopi');
+$obj->user::setfirstname("gopal");
+print($obj->user::getfirstname());
 ?>
-</pre>
